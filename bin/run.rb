653ce65874d0d7efc3d16a44dac27b70695fd6ca
Julia Nara <julianara888@gmail.com>
2017-09-24 03:31:27 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,6 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
+Pokemon.save("Pikachu", "electric", @db)
+pikachu_from_db = Pokemon.find(1, @db)
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db, :hp
+
+    def initialize(id:, name:, type:, db:, hp: nil)
+      @id = id
+      @name = name
+      @type = type
+      @db = db
+      @hp = hp
+    end
+
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+      poke_vals = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+      Pokemon.new(id: poke_vals[0], name: poke_vals[1], type: poke_vals[2], hp: poke_vals[3], db: db)
+    end
+
+    def alter_hp(hp, db)
+      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, @id)
+    end
 end
